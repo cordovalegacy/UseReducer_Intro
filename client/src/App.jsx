e0584baad27aa3c000491f5f7d1f258026d623cd
Brendan Cordova <cordovalegacy19@gmail.com>
2023-04-14 13:25:35 -0700

    VITE, USEREDUCER, SASS, CLEAN w/ COMMENTS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,138 +3,148 @@ import './App.css'
 
 function App() {
 
-  const initialFormState = {
-    firstName: {
+  // *****************************FEED TO USEREDUCER FUNCTION PARAMETERS**********************************
+  const initialFormState = { //HOLDS EACH INPUT FIELD AS A NESTED OBJECT
+    firstName: { //HOLDS BOTH VALUE AND ERROR KEYS AND VALUES
       value: "",
       error: ""
     },
-    lastName: {
+    lastName: { //HOLDS BOTH VALUE AND ERROR KEYS AND VALUES
       value: "",
       error: ""
     },
-    email: {
+    email: { //HOLDS BOTH VALUE AND ERROR KEYS AND VALUES
       value: "",
       error: ""
     }
   }
 
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "SET_FIRST_NAME":
-        return {
+  // *****************************FEED TO USEREDUCER FUNCTION PARAMETERS**********************************
+  // An action is JUST A JAVASCRIPT OBJECT {}, or set of key value pairs, with 2 distinct keys
+  // let action = {type: "SET_EMAIL", payload: "someone@gmail.com"}
+
+  const reducer = (state, action) => { //HOLDS CURRENT STATE, AND ACTION OBJECT
+    switch (action.type) { //ACTION OBJECT HOLDS TWO PROPERTIES: TYPE & PAYLOAD
+      case "SET_FIRST_NAME": //TYPE
+        return { //PAYLOAD
           ...state,
           firstName: {
             ...state.firstName,
             value: action.payload
           }
         }
-      case "SET_LAST_NAME":
-        return {
+      case "SET_LAST_NAME": //TYPE
+        return { //PAYLOAD
           ...state,
           lastName: {
             ...state.lastName,
             value: action.payload
           }
         }
-      case "SET_EMAIL":
-        return {
+      case "SET_EMAIL": //TYPE
+        return { //PAYLOAD
           ...state,
           email: {
             ...state.email,
             value: action.payload
           }
         }
-      case "SET_FIRST_NAME_ERROR":
-        return {
+      case "SET_FIRST_NAME_ERROR": //TYPE
+        return { //PAYLOAD
           ...state,
           firstName: {
             ...state.firstName,
             error: action.payload
           }
         }
-      case "SET_LAST_NAME_ERROR":
-        return {
+      case "SET_LAST_NAME_ERROR": //TYPE
+        return { //PAYLOAD
           ...state,
           lastName: {
             ...state.lastName,
             error: action.payload
           }
         }
-      case "SET_EMAIL_ERROR":
-        return {
+      case "SET_EMAIL_ERROR": //TYPE
+        return { //PAYLOAD
           ...state,
           email: {
             ...state.email,
             error: action.payload
           }
         }
-      default:
-        return state
+      default: //TYPE
+        return state //PAYLOAD
     }
   }
 
-  const [state, dispatch] = useReducer(reducer, initialFormState)
+  // *****************************************USEREDUCER HOOK*********************************************
 
-  const changeHandler = (e) => {
+  const [state, dispatch] = useReducer(reducer, initialFormState) //USEREDUCER HOOK (MUST BE BELOW REDUCER AND INITAL STATE)
+
+  // *****************************CHANGE HANDLER FUNCTION (FIELD: VALUES & ERRORS)************************
+
+  const changeHandler = (e) => { //UPDATES STATE DEPENDENT ON E.TARGET.NAME AND E.TARGET.VALUE
     console.log(e)
-    //if condition is true, it will run through TUNNEL ONE, if not, TUNNEL TWO
-    if (e.target.value.length < 3) {
-      // TUNNEL ONE
-      if (e.target.name === "firstName") {
-        dispatch({
+    //IF CONDITION IS TRUE, IT WILL RUN *TUNNEL ONE* (ERROR HANDLER)
+    //IF CONDITION IS NOT TRUE IT WILL RUN *TUNNEL TWO* (VALUE HANDLER)
+    if (e.target.value.length < 3) { //NAME AITTRIBUTE ON INPUT FIELDS
+      // TUNNEL ONE (ERROR HANDLER)
+      if (e.target.name === "firstName") { //SETTING FIRSTNAME ERRORS
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_FIRST_NAME_ERROR",
           payload: "First Name Must Be At Least 3 Characters"
         })
       }
-      else if (e.target.name === "lastName") {
-        dispatch({
+      else if (e.target.name === "lastName") { //SETTING LASTNAME ERRORS
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_LAST_NAME_ERROR",
           payload: "Last Name Must Be At Least 3 Characters"
         })
       }
-      else if (e.target.name === "email") {
-        dispatch({
+      else if (e.target.name === "email") { //SETTING EMAIL ERRORS
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_EMAIL_ERROR",
           payload: "Email Address Must Be Valid"
         })
       }
     }
-    // TUNNEL TWO
+    // TUNNEL TWO (VALUE HANDLER)
     else {
-      if (e.target.name === "firstName") {
-        dispatch({
+      if (e.target.name === "firstName") { //SETTING FIRSTNAME VALUES AND CLEARING ERRORS
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_FIRST_NAME",
           payload: e.target.value
         })
-        dispatch({
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_FIRST_NAME_ERROR",
           payload: ""
         })
       }
-      if (e.target.name === "lastName") {
-        dispatch({
+      if (e.target.name === "lastName") { //SETTING FIRSTNAME VALUES AND CLEARING ERRORS
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_LAST_NAME",
           payload: e.target.value
         })
-        dispatch({
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_LAST_NAME_ERROR",
           payload: ""
         })
       }
-      if (e.target.name === "email") {
-        dispatch({
+      if (e.target.name === "email") { //SETTING FIRSTNAME VALUES AND CLEARING ERRORS
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_EMAIL",
           payload: e.target.value
         })
-        dispatch({
+        dispatch({ //SETTER (USES ACTION OBJECT FROM REDUCER FUNCTION: TYPE, PAYLOAD)
           type: "SET_EMAIL_ERROR",
           payload: ""
         })
       }
     }
   }
-  // An action is JUST A JAVASCRIPT OBJECT {}, or set of key value pairs, with 2 distinct keys
-  // let action = {type: "SET_EMAIL", payload: "someone@gmail.com"}
+
+  // **********************************************JSX*******************************************
 
   return (
     <div className="App">
@@ -152,7 +162,7 @@ function App() {
             name="firstName"
             onChange={(e) => changeHandler(e)}
           />
-          {state.firstName.error !== "" ? <p>{state.firstName.error}</p>: null}
+          {state.firstName.error !== "" ? <p>{state.firstName.error}</p> : null}
         </div>
         <div>
           <label htmlFor="lastName">Last Name</label>
@@ -161,7 +171,7 @@ function App() {
             name="lastName"
             onChange={(e) => changeHandler(e)}
           />
-          {state.lastName.error !== "" ? <p>{state.lastName.error}</p>: null}
+          {state.lastName.error !== "" ? <p>{state.lastName.error}</p> : null}
         </div>
         <div>
           <label htmlFor="email">Email</label>
@@ -170,7 +180,7 @@ function App() {
             name="email"
             onChange={(e) => changeHandler(e)}
           />
-          {state.email.error !== "" ? <p>{state.email.error}</p>: null}
+          {state.email.error !== "" ? <p>{state.email.error}</p> : null}
         </div>
         <input type="submit" value="Submit" />
       </form>
